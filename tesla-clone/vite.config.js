d72b52efde084b13862b230637db7857a0d92f93
Khizar Sheikh <khizarsheikh.012@gmail.com>
2024-01-18 19:48:49 +0500

    add netlify

--- tesla-clone/vite.config.js
+++ tesla-clone/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vitePluginPages from "vite-plugin-gh-pages";
 
 export default defineConfig({
   base: "./", // Set the base path to "./"
-  plugins: [react(), vitePluginPages],
+  plugins: [react()],
 });
